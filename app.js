86eea17b9751b33b57ae8c5e681bb4b301dce8de
adhlira <adhliramadhan68@gmail.com>
2024-11-18 17:36:37 +0700

    add route categories

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import sign_up_route from "./routes/sign_up.js";
 import login_route from "./routes/login.js";
 import product_route from "./routes/product.js";
+import categories_route from "./routes/categories.js";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(sign_up_route);
 app.use(login_route);
 app.use(product_route);
+app.use(categories_route);
 
 export default app;
---
+++ routes/categories.js
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import { Router } from "express";
+import { authToken, authorizePermission } from "../authenticate_token.js";
+import { Permission } from "../authorization.js";
+
+const prisma = new PrismaClient();
+const router = Router();
+
+router.use(authToken);
+
+router.get("/categories", authorizePermission(Permission.BROWSE_CATEGORIES), async (req, res) => {
+  try {
+    const categories = await prisma.categories.findMany();
+    res.status(200).json({ message: "Data categories : ", categories });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/category/:id", authorizePermission(Permission.READ_CATEGORIES), async (req, res) => {
+  try {
+    const category = await prisma.categories.findFirst({ where: { id: +req.params.id } });
+    if (isNaN(req.params.id)) {
+      res.status(400).json({ message: "Invalid ID" });
+    } else if (!category) {
+      res.status(404).json({ message: "Category Not Found" });
+    } else {
+      res.status(200).json({ message: "Data Category : ", category });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
